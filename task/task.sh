d1255200cbb1003c426f28bc67569399cccadd0e
vipinkumar2 <vipin@noborderz.com>
2022-12-07 14:15:37 +0530

    add crontab sh file

---
+++ task/task.sh
@@ -0,0 +1,13 @@
+
+export CURRENT_DIR=`dirname $(readlink -f $0)`
+export PRJ_DIR=`dirname $CURRENT_DIR`
+# go to project root directory
+cd $PRJ_DIR
+. env/bin/activate
+cd src/
+amazon.ae
+firstcry.ae
+lego.saudiblocks.com
+lego.yellowblocks.me
+toysrusmena.com
+
